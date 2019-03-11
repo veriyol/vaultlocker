b8ebd0d4430b078c99e5114302a6768f2547c2e7
Çağatay Yüksel <cagatay@cagatay.co.uk>
2019-03-11 14:45:09 +0300

    Respect VAULT_SKIP_VERIFY

--- vaultlocker/shell.py
+++ vaultlocker/shell.py
@@ -37,7 +37,8 @@ def _vault_client(config):
     :param: config: configparser object of vaultlocker config
     :returns: hvac.Client. configured Vault Client object
     """
-    client = hvac.Client(url=config.get('vault', 'url'))
+    verify_tls = not bool(int(os.getenv("VAULT_SKIP_VERIFY", 0))
+    client = hvac.Client(url=config.get('vault', 'url'), verify=verify_tls)
     client.auth_approle(config.get('vault', 'approle'),
                         secret_id=config.get('vault', 'secret_id'))
     return client
